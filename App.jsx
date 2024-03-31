c13059c981d55588225cd304adaa6191862a995e
sh1vrm <sanketh.mekala@edu.sait.ca>
2024-03-30 22:39:02 -0600

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
